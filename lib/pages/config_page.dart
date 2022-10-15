a6eb18a453db7bb859e77c9fc3511a3b09f9eaf2
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2022-10-14 20:34:03 -0500

    Create config_page widget

---
+++ lib/pages/config_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ConfigPage extends StatelessWidget {
+  const ConfigPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
