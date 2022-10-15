b5284c0dc52bcbb2a303fa12e6b07e313b056dd3
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2022-10-14 22:37:37 -0500

    Delete debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'College expenses',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
