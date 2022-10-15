5dfb8f112bd9beff99d5f788efb4ec364b08df6f
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2022-10-14 21:47:24 -0500

    Add double ride checkbox

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,15 +1,29 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
+
+  @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  bool _isDoubleRide = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Settings")),
+      body: Column(children: [
+        CheckboxListTile(
+          value: _isDoubleRide,
+          onChanged: (value) => setState(() {
+            _isDoubleRide = value ?? false;
+          }),
+          title: const Text("Double ride"),
+          controlAffinity: ListTileControlAffinity.leading,
+        ),
+      ]),
     );
   }
 }
