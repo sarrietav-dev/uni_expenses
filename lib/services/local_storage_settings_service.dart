2e8c76f242151dab3ff8561ccd81c1b7032363d7
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2022-10-17 01:00:56 -0500

    Use ChangeNotifier in the SettingService

--- lib/services/local_storage_settings_service.dart
+++ lib/services/local_storage_settings_service.dart
@@ -1,23 +1,34 @@
 import 'dart:convert';
 
+import 'package:flutter/cupertino.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:uni_expenses/models/setting_context.dart';
 import 'package:uni_expenses/services/settings_service.dart';
 
-class LocalStorageSettingService implements ISettingsService {
+class LocalStorageSettingService extends ChangeNotifier
+    implements ISettingsService {
+  late SettingContext _settings;
+
   @override
   Future<SettingContext> getSettings() async {
     var sharedPreferences = await SharedPreferences.getInstance();
-    return sharedPreferences.get("settings") != null
+    var settings = sharedPreferences.get("settings") != null
         ? SettingContext.fromJson(
             object: jsonDecode(sharedPreferences.get("settings").toString())
                 as Map<String, dynamic>)
         : SettingContext(busPrice: 2700, isDoubleRide: false);
+
+    _settings = settings;
+
+    notifyListeners();
+    return _settings;
   }
 
   @override
   Future<void> saveSettings(SettingContext settings) async {
     var sharedPreferences = await SharedPreferences.getInstance();
     sharedPreferences.setString("settings", jsonEncode(settings.toJson()));
+    _settings = settings;
+    notifyListeners();
   }
 }
